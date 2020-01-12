297c407dec10898b2a3a994f40a859778586266f
sergeantIbra <54941210+sergeantChill@users.noreply.github.com>
2020-01-12 21:10:29 +0300

    Initial commit

---
+++ Assets/Scripts/FauxGravityBody.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class FauxGravityBody : MonoBehaviour
+{
+
+    private FauxGravityAttractor attractor;
+    private Rigidbody rb;
+
+    public bool placeOnSurface = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        attractor = FauxGravityAttractor.instance;
+    }
+
+    void FixedUpdate()
+    {
+        if (placeOnSurface)
+            attractor.PlaceOnSurface(rb);
+        else
+            attractor.Attract(rb);
+    }
+
+}
