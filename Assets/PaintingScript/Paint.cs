9e43cf86c363889f956f685a409b4a430e7e06c0
sergeantIbra <54941210+sergeantChill@users.noreply.github.com>
2020-02-21 00:37:38 +0300

    Реализация идеи с постройкой зданий

--- Assets/PaintingScript/Paint.cs
+++ Assets/PaintingScript/Paint.cs
@@ -1,19 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class Paint : MonoBehaviour {
 
 
     public GameObject brush;
     public GameObject player;
     public PlayerCollision playerCollision;
+    private Vector3 spawnPos;
+
+    public GameObject loseText;
+    public bool GameOver;
 
     private void Start()
     {
         StartCoroutine(Build());
     }
     void Update () {
+        if (GameOver)
+        {
+            StartCoroutine(reloadGame());
+            player.GetComponent<PlayerController>().moveSpeed = 0;
+
+        }
     }
     public void Painting(GameObject brushPrefab)
     {
@@ -22,16 +33,48 @@ public class Paint : MonoBehaviour {
         {
             Vector3 pos = new Vector3(hit.point.x, hit.point.y, hit.point.z) + hit.normal * 0.1f;
             GameObject building = Instantiate(brushPrefab, pos, Quaternion.FromToRotation(Vector3.forward * -1.0f, hit.normal));
-            building.transform.position = pos;
+            StartCoroutine(scale(building, Random.Range(0.2f,1.3f)));
+            StartCoroutine(enableCollision(building));
+            
         }
+    }
+
+    IEnumerator scale(GameObject prefab, float scaleSize)
+    {
+        for(float i = 0; i < scaleSize; i+=0.1f)
+        {
+            prefab.transform.localScale = new Vector3(prefab.transform.localScale.x, prefab.transform.localScale.y,i);
+            yield return new WaitForSeconds(0.1f);
+        }
+
     }
 
     IEnumerator Build()
     {
-        while (true)
+        while (!GameOver)
         {
             Painting(brush);
-            yield return new WaitForSeconds(0.1f);
+            yield return new WaitForSeconds(0.5f);
         }
     }
+
+    IEnumerator enableCollision(GameObject prefab)
+    {
+        yield return new WaitForSeconds(0.5f);
+        prefab.AddComponent<BuildingCollision>();
+
+    }
+
+    IEnumerator reloadGame()
+    {
+        loseText.SetActive(true);
+        for (byte opasity = 0; opasity != 255; opasity++)
+        {
+            yield return new WaitForSeconds(0.0001f);
+            loseText.GetComponent<Text>().color = new Color32(0, 0, 0, opasity);
+        }
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+
 }
---
+++ Assets/Scripts/BuildingCollision.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class BuildingCollision : MonoBehaviour
+{
+    bool gameOver = false;
+    public GameObject Builder;
+    private void Start()
+    {
+        Builder = GameObject.FindWithTag("MainCamera");
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("You DEAD");
+            Builder.GetComponent<Paint>().GameOver = true;
+        }
+    }
+
+  
+}
